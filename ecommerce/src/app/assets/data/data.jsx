8484e5923e88d892b8c3ce3f47c0974a9caeabf5
egunfavour <egunfavour@gmail.com>
2022-11-27 23:05:16 +0100

    images

--- ecommerce/src/app/assets/data/data.jsx
+++ ecommerce/src/app/assets/data/data.jsx
@@ -1,6 +1,10 @@
 import { BsCurrencyDollar } from "react-icons/bs";
 import { TbCurrencyEuro } from "react-icons/tb";
 import { BsCurrencyYen } from "react-icons/bs";
+import apollo from '../img/image.jpg'
+import short from '../img/image1.jpg'
+import running from '../img/image2.jpg'
+import shirt from '../img/image2,jpg'
 
 export const dropdownItems = [
   {
@@ -18,4 +22,33 @@ export const dropdownItems = [
     icon: <BsCurrencyYen />,
     currency: "JPY",
   },
+];
+
+
+
+export const StoreItems = [
+  {
+    id: 1,
+    Image: apollo,
+    name: "Apollo Running Short",
+    price: "USD",
+  },
+  {
+    id: 2,
+    image: short,
+    name: "Apollo Running Short",
+    currency: "USD",
+  },
+  {
+    id: 3,
+    image: running,
+    name: "Apollo Running Short",
+    currency: "USD",
+  },
+  {
+    id: 4,
+    image: shirt,
+    name: "Apollo Running Short",
+    currency: "USD",
+  }
 ];
