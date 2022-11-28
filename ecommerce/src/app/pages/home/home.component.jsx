06559fab26ec1d9643dabc06f5e10a99e48c1fd9
Oluwatimi11 <masterarcher6321@gmail.com>
2022-11-28 01:16:36 +0100

    Update

--- ecommerce/src/app/pages/home/home.component.jsx
+++ ecommerce/src/app/pages/home/home.component.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import CategoryHeading from '../../components/molecules/category-head/category-head.component'
 import Navbar from '../../components/molecules/navbar/navbar.components'
+import Main from '../../components/molecules/main/main'
 
 const Home = () => {
   return (
     <div>
         <Navbar />
         <CategoryHeading />
+        <Main />
     </div>
   )
 }
